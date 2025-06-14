c8bdb12d19765f29a51d88f527208256d005ce64
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2025-06-14 12:40:59 -0300

    adding page

--- web_personal_teacher/src/app/app.module.ts
+++ web_personal_teacher/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionCreatorComponent } from './componentes/question-creator/question-creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionCreatorComponent
   ],
   imports: [
     BrowserModule,
---
+++ web_personal_teacher/src/app/componentes/question-creator/question-creator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-creator',
+  standalone: false,
+  templateUrl: './question-creator.component.html',
+  styleUrl: './question-creator.component.css'
+})
+export class QuestionCreatorComponent {
+
+}
